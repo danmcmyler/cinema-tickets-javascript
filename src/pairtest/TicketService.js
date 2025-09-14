bfc7635f51e0062e10a6fb3710a0ecb3800881cb
Dan McMyler <danmcmyler@gmail.com>
2025-09-14 17:55:55 +0100

    Added adult ticket purchase logic

--- src/pairtest/TicketService.js
+++ src/pairtest/TicketService.js
@@ -1,12 +1,41 @@
 import TicketTypeRequest from './lib/TicketTypeRequest.js';
 import InvalidPurchaseException from './lib/InvalidPurchaseException.js';
+import TicketPaymentService from '../thirdparty/paymentgateway/TicketPaymentService.js';
+import SeatReservationService from '../thirdparty/seatbooking/SeatReservationService.js';
 
 export default class TicketService {
+  constructor(
+    paymentService = new TicketPaymentService(),
+    seatService = new SeatReservationService()
+  ) {
+    this._paymentService = paymentService;
+    this._seatService = seatService;
+  }
+
   /**
    * Should only have private methods other than the one below.
    */
-
   purchaseTickets(accountId, ...ticketTypeRequests) {
-    // throws InvalidPurchaseException
+
+    let adults = 0;
+
+    for (const req of ticketTypeRequests) {
+      if (
+        req &&
+        typeof req.getTicketType === 'function' &&
+        typeof req.getNoOfTickets === 'function' &&
+        req.getTicketType() === 'ADULT'
+      ) {
+        adults += req.getNoOfTickets();
+      }
+    }
+
+    const totalToPay = adults * 25;
+    const seatsToReserve = adults;
+
+    this._paymentService.makePayment(accountId, totalToPay);
+    this._seatService.reserveSeat(accountId, seatsToReserve);
+
+    return { accountId, totalToPay, seatsToReserve };
   }
 }
